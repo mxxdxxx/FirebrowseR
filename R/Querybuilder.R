fec2c48871b2d9781a2fec5c3f59ce88d9736c29
Mario Deng <mariodeng@googlemail.com>
2015-05-16 11:08:43 +0200

    Updated to v1.1.6 beta

--- R/Querybuilder.R
+++ R/Querybuilder.R
@@ -10,7 +10,7 @@
 #'
 #' @export
 API.Version = function(){
-  number = "1.1.3"
+  number = "1.1.6"
   beta = T
   ret = ifelse(beta, paste("v", number, ":beta", sep=""), paste("v", number, sep=""))
   return(ret)
--- tests/testthat/test.Analyses.FeatureTable.R
+++ tests/testthat/test.Analyses.FeatureTable.R
@@ -9,11 +9,13 @@ test_that("URLs are retrieved correctly", {
   page = 1
   page_size = 250
 
-  expect_error(Analyses.FeatureTable(format = format,
-                                  cohort = cohort,
-                                  column = column,
-                                  page = page,
-                                  page_size = page_size))
+  obj = Analyses.FeatureTable(format = format,
+                              cohort = cohort,
+                              column = column,
+                              page = page,
+                              page_size = page_size)
+  expect_equal(nrow(obj), 250)
+  expect_equal(ncol(obj), 2)
 
   column = ""
   obj = Analyses.FeatureTable(format = format,
