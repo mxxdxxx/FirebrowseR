48c9f8769743ef998dab1c4517089e90ecbafca7
Mario Deng <mariodeng@googlemail.com>
2015-05-19 15:48:55 +0200

    Updated to API v1.1.7 beta

--- R/Querybuilder.R
+++ R/Querybuilder.R
@@ -10,7 +10,7 @@
 #'
 #' @export
 API.Version = function(){
-  number = "1.1.6"
+  number = "1.1.7"
   beta = T
   ret = ifelse(beta, paste("v", number, ":beta", sep=""), paste("v", number, sep=""))
   return(ret)
