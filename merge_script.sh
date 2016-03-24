d7dab4e3fd796443a784630a64d531f3b3d32e85
Mario Deng <mariodeng@googlemail.com>
2016-03-24 20:00:44 +0100

    fixed merged script

--- merge_script.sh
+++ merge_script.sh
@@ -6,8 +6,8 @@ fi
 #export GIT_COMMITTER_NAME="Mario Deng"
 
 git init || exit
-git config user.name "Travis CI" || exit
+git config user.name "Travis-CI" || exit
 git config user.email "mariodeng@googlemail.com" || exit
 git add . || exit
 git commit -m "auto deploy from travis after update" || exit
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" swagger:master > /dev/null 2>&1 || exit
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1 || exit
