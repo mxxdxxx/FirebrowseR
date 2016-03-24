8a73971a10bc8eb5c37216f25c9d8357e84afea3
Mario Deng <mariodeng@googlemail.com>
2016-03-24 16:49:24 +0100

    preparing for auto mege

---
+++ merge_script.sh
@@ -0,0 +1,10 @@
+if [ "$TRAVIS_BRANCH" != "test" ]; then
+  exit 0;
+fi
+    
+export GIT_COMMITTER_EMAIL=i"mariodeng@googlemail.com"
+export GIT_COMMITTER_NAME="Mario Deng"
+
+git checkout master || exit
+git merge "$TRAVIS_COMMIT" || exit
+git push origin master
