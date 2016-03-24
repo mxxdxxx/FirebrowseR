237f14d0499362c3bf2d425e517a489efc73357d
Mario Deng <mariodeng@googlemail.com>
2016-03-24 18:24:33 +0100

    change permissions on merge_script.sh

--- merge_script.sh
+++ merge_script.sh
@@ -5,6 +5,7 @@ fi
 export GIT_COMMITTER_EMAIL=i"mariodeng@googlemail.com"
 export GIT_COMMITTER_NAME="Mario Deng"
 
-git checkout master || exit
+#git checkout master || exit
+git checkout -b master --track origin/master
 git merge "$TRAVIS_COMMIT" || exit
 git push origin master
