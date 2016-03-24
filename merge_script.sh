36dc9bd5da4e86e9a4af6d8fa486ea31cbb18fe2
Mario Deng <mariodeng@googlemail.com>
2016-03-24 19:25:52 +0100

    updated merge script with auth

--- merge_script.sh
+++ merge_script.sh
@@ -5,9 +5,9 @@ fi
 export GIT_COMMITTER_EMAIL=i"mariodeng@googlemail.com"
 export GIT_COMMITTER_NAME="Mario Deng"
 
-#git checkout master || exit
-git remote update || exit
-git fetch || exit
-git checkout -b master --track origin/master || exit
-git merge "$TRAVIS_COMMIT" || exit
-git push origin master
+git init || exit
+git config user.name "Travis CI" || exit
+git config user.email "mariodeng@googlemail.com" || exit
+git add . || exit
+git commit -m "auto deploy from travis after update" || exit
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" swagger:master > /dev/null 2>&1}} || exit
