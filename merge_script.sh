eadfa0e9afa28b20f8a6583b08a2a8c961d2bd27
Mario Deng <mariodeng@googlemail.com>
2016-11-22 14:15:14 +0100

    security update for travis token

--- merge_script.sh
+++ merge_script.sh
@@ -1,11 +1,11 @@
 if [ "$TRAVIS_BRANCH" != "swagger" ]; then
-  exit 0; 
+  exit 0;
 fi
-    
+
 git init || exit
 git config user.name "Travis-CI" || exit
 git config user.email "mariodeng@googlemail.com" || exit
 git add . || exit
 git commit -m "Auto deploy from travis after api update" || exit
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" swagger:master  || exit
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" swagger:master > /dev/null 2>&1 || exit
 
